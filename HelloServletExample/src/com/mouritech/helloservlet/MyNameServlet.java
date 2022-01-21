cf25a4c11409965ae0019f22f3fd0555842fd5b5
RupaPolipilliMouri <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-01-22 01:21:06 +0530

    Add files via upload

---
+++ HelloServletExample/src/com/mouritech/helloservlet/MyNameServlet.java
@@ -0,0 +1,47 @@
+package com.mouritech.helloservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class MyNameServlet
+ */
+@WebServlet("/MyNameServlet")
+public class MyNameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    /**public MyNameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }*/
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// set response content type
+		
+		  response.setContentType("text/html");
+		  
+		  //print the text on the browser
+		  
+		 PrintWriter out = response.getWriter();
+		//read the data entered in the html
+			String enteredName = request.getParameter("MyName");
+			out.println("<h1> Enter Name = " +enteredName + "</h1>");
+	}
+
+}
