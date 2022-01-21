cd9a54bb23db278f54f10cd429d22cbec483dbf4
RupaPolipilliMouri <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-01-22 02:21:32 +0530

    Add files via upload

---
+++ DatabaseProductData.sql
@@ -0,0 +1,11 @@
+use mouritech;
+CREATE TABLE product
+(
+Productid int(10),
+ProductName varchar(25),
+ProductPrice float(25),
+ProductBrand varchar(25),
+ProductCategory varchar(25)
+);
+desc Product;
+select * from Product;
